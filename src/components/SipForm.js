10ad2e808824bfa7ec183a9f4fedb25e696fa271
Ranjeet Vishwakarma <rvishwakarma0099@gmail.com>
2024-12-28 11:47:27 +0530

    icon added

--- src/components/SipForm.js
+++ src/components/SipForm.js
@@ -61,7 +61,9 @@ function SIPForm({ onSubmit }) {
                             onClick={() => handleRemove(index)}
                             className="px-3 py-1 bg-red-500 text-white rounded-lg hover:bg-red-600 mt-2 sm:mt-0 sm:ml-4"
                         >
-                            Remove
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
                         </button>
                     )}
                 </div>
@@ -71,15 +73,22 @@ function SIPForm({ onSubmit }) {
                 <button
                     type="button"
                     onClick={handleAdd}
-                    className="px-5 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 w-full sm:w-auto"
+                    className="flex items-center space-x-2 px-3 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 w-full sm:w-auto"
                 >
-                    Add Category
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Add Category</span>
                 </button>
                 <button
                     type="submit"
-                    className="px-5 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 w-full sm:w-auto"
+                    className="flex items-center space-x-2 px-3 py-2 bg-emerald-500 text-white rounded-lg hover:bg-emerald-600 w-full sm:w-auto"
                 >
-                    Generate Chart
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+                    </svg>
+                    <span>Generate Chart</span>
                 </button>
             </div>
         </form>
