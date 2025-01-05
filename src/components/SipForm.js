cf540ddc6ccf18fda9d8ebe7d8e9d8a4528726a9
vishwakarmaranjeet <rvishwakarma0099@gmail.com>
2025-01-05 20:21:21 +0530

    placed delete icon inside input

--- src/components/SipForm.js
+++ src/components/SipForm.js
@@ -96,7 +96,7 @@ function SIPForm({ onSubmit }) {
 
     return (
         <form onSubmit={handleSubmit} className="bg-white rounded-lg p-6 w-full max-w-2xl mx-auto">
-            <h2 className="text-2xl font-semibold text-gray-700 mb-6">Enter SIP Amount</h2>
+            <h2 className="text-1xl font-semibold text-gray-700 mb-6">Enter SIP Amount</h2>
 
             {sipList.map((item, index) => (
                 <div key={index} className="flex flex-col sm:flex-row sm:space-x-4 mb-4">
@@ -121,26 +121,29 @@ function SIPForm({ onSubmit }) {
                         required
                         className="w-full sm:w-1/2 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400 mb-2 sm:mb-0"
                     /> */}
-                    <input
-                        type="text"
-                        name="amount"
-                        placeholder="Amount"
-                        value={`₹ ${new Intl.NumberFormat().format(item.amount || 0)}`}
-                        onChange={(e) => handleChange(index, e)}
-                        required
-                        className="w-full sm:w-1/2 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400 mt-3 sm:mt-0 sm:ml-4"
-                    />
-                    {sipList.length > 1 && (
-                        <button
-                            type="button"
-                            onClick={() => handleRemove(index)}
-                            className="flex items-center justify-center px-2 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 w-fit sm:w-auto mt-2 sm:mt-0 sm:ml-4"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.1} stroke="currentColor" className="w-5 h-5 sm:w-6 sm:h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                            </svg>
-                        </button>
-                    )}
+                    <div className="relative w-full sm:w-1/2 mt-3 sm:mt-0">
+                        <input
+                            type="text"
+                            name="amount"
+                            placeholder="Amount"
+                            value={`₹ ${new Intl.NumberFormat().format(item.amount || 0)}`}
+                            onChange={(e) => handleChange(index, e)}
+                            required
+                            className="w-full px-4 py-2 pr-12 border rounded-lg focus:ring-2 focus:ring-blue-400"
+                        />
+
+                        {sipList.length > 1 && (
+                            <button
+                                type="button"
+                                onClick={() => handleRemove(index)}
+                                className="absolute inset-y-0 right-0 px-3 flex items-center bg-red-500 text-white rounded-r-lg hover:bg-red-600"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+                            </button>
+                        )}
+                    </div>
                 </div>
             ))}
 
@@ -148,7 +151,7 @@ function SIPForm({ onSubmit }) {
                 <button
                     type="button"
                     onClick={handleAdd}
-                    className="flex items-center justify-center space-x-2 px-3 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 w-half sm:w-auto"
+                    className="flex text-sm items-center justify-center space-x-2 px-3 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 w-half sm:w-auto"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
