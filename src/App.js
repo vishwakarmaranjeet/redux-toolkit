f38c9ed13079d7125435f068cc77d7af503061c5
vishwakarmaranjeet <rvishwakarma0099@gmail.com>
2025-03-14 18:39:21 +0530

    update index.html title and remove unused assets; refactor SIP calculation logic and add product data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Counter from './counter/Counter';
-// import User from "./User/User";
-// import FilterableProductTable from './ProductFilter/FilterableProductTable';
 import NavHeader from './components/NavHeader';
 import CAGRCalculator from './components/CagrCalculator';
 import Home from './components/Home';
 import PageNotFound from './components/PageNotFound';
-
-// const PRODUCTS = [
-//   { category: "Fruits", price: "$3", stocked: true, name: "Apple" },
-//   { category: "Fruits", price: "$2", stocked: true, name: "Dragonfruit" },
-//   { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-//   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-//   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-//   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-// ];
+// import Counter from './counter/Counter';
+// import User from "./User/User";
+// import FilterableProductTable from './ProductFilter/FilterableProductTable';
 
 function App() {
   return (
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,9 @@
-export const calculateSIPPercentages = (sipDetails)=> {
-    const totalSIP = Object.values(sipDetails).reduce((acc, sip) =>  acc + sip, 0);
-
-    for(const [sipType, amount] of Object.entries(sipDetails)){
+export const calculateSIPPercentages = (sipDetails) => {
+    const totalSIP = Object.values(sipDetails).reduce((acc, sip) => acc + sip, 0);
+    for (const [sipType, amount] of Object.entries(sipDetails)) {
         const percentage = (amount / totalSIP) * 100;
         console.log(`${sipType}: ${percentage.toFixed(2)}%`)
     }
-    console.log(`Total Sip amount: ${totalSIP}`);
 }
 
 export const SIP_DETAILS = {
@@ -43,4 +41,13 @@ export const CATEGORIES = [
     "Dividend Yield Fund",
     "Sectoral Thematic fund",
     "Index Fund",
+];
+
+export const PRODUCTS = [
+    { category: "Fruits", price: "$3", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$2", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
 ];
